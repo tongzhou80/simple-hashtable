153e4e7997180cddace1e521096c71979966334a
Tong Zhou <zt9465@gmail.com>
2025-03-16 15:48:15 -0400

    added a rehash function

--- simple_hashtable.py
+++ simple_hashtable.py
@@ -33,8 +33,10 @@ def _probe(key, table):
 
 
 @njit
-def _put(key, val, table):
+def _put(key, val, table, usage_info):
     i = _probe(key, table)
+    if table[i]['status'] != 1:
+        usage_info[0] += 1
     table[i]['status'] = 1
     table[i]['key'] = key
     table[i]['val'] = val
@@ -56,27 +58,48 @@ def _has(key, table):
 
 
 @njit
-def _delete(key, table):
+def _delete(key, table, usage_info):
     i = _probe(key, table)
+    if table[i]['status'] == 1:
+        usage_info[0] -= 1
+        usage_info[1] += 1
     table[i]['status'] = 2
+
+
+@njit
+def _rehash(old_table, new_table, old_usage_info, new_usage_info):
+    for i in range(len(old_table)):
+        if old_table[i]['status'] == 1:
+            _put(old_table[i]['key'], old_table[i]['val'], new_table, new_usage_info)
 
 
 class SimpleDict:
     def __init__(self, val_dtype, size=8):
-        self._val_dtype = val_dtype
-        self._table = _init(val_dtype, size)
+        self.val_dtype = val_dtype
+        self.table = _init(val_dtype, size)
+        # (num_of_occupied_buckets, num_of_deleted_buckets)
+        self.usage_info = [0, 0]
 
     def put(self, key, val):
-        _put(key, val, self._table)
+        _put(key, val, self.table, self.usage_info)
+        if (sum(self.usage_info) / len(self.table)) > 0.7:
+            self.rehash()
 
     def get(self, key, default=None):
-        return _get(key, self._table, default)
+        return _get(key, self.table, default)
 
     def has(self, key):
-        return _has(key, self._table)
+        return _has(key, self.table)
 
     def delete(self, key):
-        _delete(key, self._table)
+        _delete(key, self.table, self.usage_info)
+
+    def rehash(self):
+        new_table = _init(self.val_dtype, len(self.table) * 4)
+        new_usage_info = [0, 0]
+        _rehash(self.table, new_table, self.usage_info, new_usage_info)
+        self.table = new_table
+        self.usage_info = new_usage_info
 
     def __getitem__(self, key):
         return self.get(key)
@@ -88,6 +111,6 @@ class SimpleDict:
         return self.has(key)
     
     def __len__(self):
-        return sum(self._table['status'] == 1)
+        return self.usage_info[0]
     
 
--- test.py
+++ test.py
@@ -2,20 +2,22 @@ import numpy as np
 from simple_hashtable import SimpleDict
 
 np.random.seed(0)
-d = SimpleDict(size=20003, val_dtype=np.int32)
+d = SimpleDict(val_dtype=np.int32)
 N = 10000
 keys = np.random.randint(10000, size=N)
 vals = np.random.randint(10000, size=N)
 for k, v in zip(keys, vals):
     d[k] = v
-    if k % 3 == 0:
+    if k % 2 == 0:
         d.delete(k)
     
 d1 = {}
 for k, v in zip(keys, vals):
     d1[k] = v
-    if k % 3 == 0:
+    if k % 2 == 0:
         del d1[k]
+
+print(len(d), len(d1))
 
 for k, v in zip(keys, vals):
     if d.get(k, None) != d1.get(k, None):
